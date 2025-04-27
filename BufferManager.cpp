59b839f6d1bf977088c0dd97b634fe5487c0c291
abhimanyusk13 <50130341+abhimanyusk13@users.noreply.github.com>
2025-04-27 14:23:30 +0530

    Create BufferManager.cpp

---
+++ BufferManager.cpp
@@ -0,0 +1,12 @@
+// File: BufferManager.cpp
+#include "BufferManager.h"
+
+BufferManager::BufferManager() {}
+
+char *BufferManager::fetchPage(int pageId) {
+    // TODO
+    return nullptr;
+}
+
+void BufferManager::pinPage(int pageId) {}
+void BufferManager::unpinPage(int pageId) {}
